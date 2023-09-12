cc224d1bf245b7b7eedc941612c45a9df3c4bbaa
Deekshith-AC <85830510+Deekshith-AC@users.noreply.github.com>
2023-09-12 23:29:29 +0530

    1hr 30 mins

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-const scroll = new LocomotiveScroll({
+document.addEventListener("DOMContentLoaded", function() 
+{
+    const dateDisplay = document.getElementById("extra5");
+
+    function update() {
+        const currentDate = new Date();
+        currentDate.setDate(currentDate.getDate() + 5);
+        const options = { day: "numeric", month: "short", year: "numeric" };
+        const formattedDate = currentDate.toLocaleDateString(undefined, options);
+        dateDisplay.textContent = "From " + formattedDate;
+    }
+
+    update();
+
+    setInterval(update, 86400000);
+});
+const scroll = new LocomotiveScroll(
+{
     el: document.querySelector('#main'),
     smooth: true
 });
 
 const minicircle = document.querySelector("#minicircle");
-
-function circleMouse() {
+function circleMouse() 
+{
     const minicircle = document.querySelector("#minicircle");
     
-    window.addEventListener("mousemove", function (details) {
-        const mouseX = details.clientX;
-        const mouseY = details.clientY;
+    window.addEventListener("mousemove", function(details) 
+    {
+        details.preventDefault();
+        const r = details.clientX;
+        const s = details.clientY;
 
-        minicircle.style.left = mouseX + "px";
-        minicircle.style.top = mouseY + "px";
+        minicircle.style.left = r + "px";
+        minicircle.style.top = s + "px";
     });
 
     window.addEventListener("mouseout", function () {
@@ -24,8 +43,16 @@ function circleMouse() {
         minicircle.style.display = "block";
     });
 }
-
-  
-
 circleMouse();
-  
+
+function firstpageAnime()
+{
+    var tl = gsap.timeline();
+
+    tl.from("#nav", {
+        y: "-10",
+        duration: 2,
+        opacity: 0,
+        ease: Expo.easeInOut
+    });
+}
